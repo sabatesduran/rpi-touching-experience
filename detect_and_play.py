3bb4b0d8dd13ad53835e22f9de48ab08ffad4f08
Dídac Sabatés <sabatesduran@gmail.com>
2019-03-25 17:25:14 +0100

    Fix relative folder detect_and_play

--- detect_and_play.py
+++ detect_and_play.py
@@ -15,8 +15,8 @@ def speech(pin):
     mixer.init()
 
     # Users pygame mixer to load an mp3 file and play it
-    filename = f'../voice_files/{pin}.mp3'
-    not_found_filename = '../voice_files/not_found.mp3'
+    filename = f'./voice_files/{pin}.mp3'
+    not_found_filename = './voice_files/not_found.mp3'
 
     # Check if file exists
     exists = os.path.isfile(filename)

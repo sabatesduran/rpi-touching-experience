e68a6c70bb971b651edf9ea53e1f882e51c0711b
Dídac Sabatés <sabatesduran@gmail.com>
2019-03-26 10:05:10 +0100

    Fix last commit

--- detect_and_play.py
+++ detect_and_play.py
@@ -1,32 +1,12 @@
 #!/usr/bin/env python
 
-import os
 import sys
 import time
 import board
 import busio
 import adafruit_mpr121
-from pygame import mixer
 from lib.functions import load_json
-
-
-def speech(pin):
-    # Start pygame mixer
-    mixer.init()
-
-    # Users pygame mixer to load an mp3 file and play it
-    filename = f'./voice_files/{pin}.mp3'
-    not_found_filename = './voice_files/not_found.mp3'
-
-    # Check if file exists
-    exists = os.path.isfile(filename)
-
-    if exists:
-        mixer.music.load(filename)
-    else:
-        mixer.music.load(not_found_filename)
-
-    mixer.music.play()
+from lib.player import play
 
 
 def detectPin():
@@ -41,7 +21,7 @@ def detectPin():
             # it will return True, otherwise it will return False.
             if mpr121[i].value:
                 print(f'Input detected PIN {i} touched!')
-                speech(i)
+                play(i)
         time.sleep(0.25)  # Small delay to keep from spamming output messages.
 
 

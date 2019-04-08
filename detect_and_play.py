e4ae9e86aa2c72a949aa10e0d0d9112083bb5f9d
Dídac Sabatés <sabatesduran@gmail.com>
2019-04-08 18:38:18 +0200

    WIP

--- detect_and_play.py
+++ detect_and_play.py
@@ -8,9 +8,9 @@ import adafruit_mpr121
 from lib.functions import load_json
 from lib.player import play
 
-def calculate_average(pins, average_times):
+def calculate_average(pins):
     for i in range(12):
-        pins[i] = pins[i] / average_times
+        pins[i] = sum(pins[i]) / len(pins[i])
     return pins
     
 
@@ -20,44 +20,41 @@ def run():
     print("============================================")
     
     # GLOBAL VARIABLES
-    AVERAGE_TIMES = 20
+    AVERAGE_TIMES = 40
     
     # Count amount of touch events happening
     touch_count = 0
     average_loop_count = 1
 
     # Set sensor sensitivity
-    adafruit_mpr121.MPR121_TOUCHTH_0 = 50
-    adafruit_mpr121.MPR121_RELEASETH_0 = 51
+    #adafruit_mpr121.MPR121_TOUCHTH_0 = 50
+    #adafruit_mpr121.MPR121_RELEASETH_0 = 51
 
     # Start sensor
     i2c = busio.I2C(board.SCL, board.SDA)
     mpr121 = adafruit_mpr121.MPR121(i2c)
     
-    print("\nWAITING FOR TOUCH EVENTS...\n")
-    
     average_per_pin = False
-    total_per_pin = [0,0,0,0,0,0,0,0,0,0,0,0]
+    total_per_pin = [[],[],[],[],[],[],[],[],[],[],[],[]]
 
     while True:
         # Loop through all 12 inputs (0-11).
         for i in range(12):
             if average_loop_count <= AVERAGE_TIMES:
-                total_per_pin[i] += mpr121[i].raw_value
-                print(i, ":", mpr121[i].raw_value)
-                if average_loop_count == AVERAGE_TIMES and not average_per_pin:
-                    average_per_pin = calculate_average(total_per_pin, AVERAGE_TIMES)
+                total_per_pin[i].append(mpr121[i].raw_value)
+                if average_loop_count == AVERAGE_TIMES and i == 11 and not average_per_pin:
+                    average_per_pin = calculate_average(total_per_pin)
                     print("AVERAGE PER PIN", average_per_pin)
+                    print("\nWAITING FOR TOUCH EVENTS...\n")
             
             # Call is_touched and pass it then number of the input. If it's touched
             # it will return True, otherwise it will return False.
             if average_per_pin and mpr121[i].value:
                 touch_count += 1
-                print(f'{touch_count}: PIN {i} touched!')
+                print(f'{touch_count}: PIN {i} TH {mpr121[i].raw_value}')
                 # play(i)
         time.sleep(0.25)  # Small delay to keep from spamming output messages.
         average_loop_count += 1
-        print(total_per_pin)
 
 if __name__ == '__main__':
     run()
